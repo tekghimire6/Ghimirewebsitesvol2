56ac048b389f857a9770d4de5f629962a72d52ee
Tek Ghimire <83563771+tekghimire6@users.noreply.github.com>
2024-04-06 16:30:31 +0000

    chnges to DB

--- tekDB.py
+++ tekDB.py
@@ -1,21 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+import os
 
-# Use the provided database credentials and endpoint
-user = 'admin'
-password = 'fwelmlskfsdf'  # Use the correct password here
-endpoint = 'tek-database-carrerwebsite.cbgq48ko0bmz.us-east-1.rds.amazonaws.com'
-database_name = 'ghimireDB'
 
 # Assuming the SSL certificate path is updated and correct
 ssl_args = {'ssl': {'ca': './global-bundle.pem'}}
-#ssl_args = {'ssl': {'ca': '/Macintosh HD/Users/tekghimire/Downloads/global-bundle.pem'}}
+
 
 # Construct your connection string
-connection_string = f"mysql+pymysql://{user}:{password}@{endpoint}/{database_name}"
+connection_string = os.environ['DB_conn_string']
 
-# Create the engine with the SSL arguments hjsfadj 
-#&&dsjfkjsdf 
+
+# Create the engine with the SSL arguments hjsf
 engine = create_engine(connection_string, connect_args=ssl_args)
 
 def loadjobfromDB():
